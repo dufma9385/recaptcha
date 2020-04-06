6b21d69e18bb6f58ed0e3d0e066b3d4fb34589c7
dufma9385 <dufma9385@gmail.com>
2020-04-06 17:59:11 +0900

    passport_kakao first

---
+++ passport-kakao/routes/kakaoLogin.js
@@ -0,0 +1,2 @@
+Kakao.init('bab9c50bfe72891b05e4c86672b80acc');
+Kakao.isInitialized();
---
+++ passport-kakao/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.listen(3000, ()=>{
+    console.log("3000 ready");
+});
